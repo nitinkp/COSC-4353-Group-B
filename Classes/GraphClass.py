0e4f69a37aa9966263cbbd2d2e257b2f730460b3
myannuzzi <mikeyannuzzi321@gmail.com>
2021-11-29 11:27:25 -0600

    fixing syntax error

--- Classes/GraphClass.py
+++ Classes/GraphClass.py
@@ -6,7 +6,7 @@ class Graph:
 	def __init__(self, name, adjMatrix):
 		self.name = name
 		self.adjMatrix = adjMatrix
-		nodeCount = self.getNodeCount
+		nodeCount = self.getNodeCount()
 	# Set name
 	# Set's name of graph
 	def setName(self,name):
@@ -35,7 +35,7 @@ class Graph:
 	# Inserts edge into graph (needs a source node and destination node, undirected or directed, can be self referencing)
 	def insertEdge(self, a, b):
 		# Check if the edge values are out of place first
-		if(a > self.no)
+		
 		self.adjMatrix[a,b] = 1
 		self.adjMatrix[b,a] = 1
 	# Remove node
