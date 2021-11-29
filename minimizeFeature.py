c9fe6a48db1c10b55f706444d59727c123eda83e
Nithin <pnithinkumar99@gmail.com>
2021-11-28 19:09:19 -0600

    Modified feature 2

--- minimizeFeature.py
+++ minimizeFeature.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 
 INF = 999
 # number of vertices in graph
@@ -7,11 +8,59 @@ nodes = int(input("Enter number of nodes: "))
 # creating graph by adjacency matrix method
 edges = int(input("Enter number of edges: "))
 
-g = np.array([[0, 19, 5, 0, 0],
-              [19, 0, 5, 9, 2],
-              [5, 5, 0, 1, 6],
-              [0, 9, 1, 0, 1],
-              [0, 2, 6, 1, 0]])
+# g = np.array([[0, 19, 5, 0, 0],
+#               [19, 0, 5, 9, 2],
+#               [5, 5, 0, 1, 6],
+#               [0, 9, 1, 0, 1],
+#               [0, 2, 6, 1, 0]])
+
+input_folder = 'Minimizing-inputs'
+
+inputs_list = []
+
+for f1, f2, f3 in os.walk(input_folder):
+    for file in f3:
+        inputs_list.append(os.path.join(f1, file))
+
+# print(inputs_list)
+given_num = int(input("choose one file from the available inputs as a number between 0 - 4: "))
+
+file_name = ""
+
+for i in range(given_num):
+    if i == 0:
+        file_name = inputs_list[0]
+    elif i == 1:
+        file_name = inputs_list[1]
+    elif i == 2:
+        file_name = inputs_list[2]
+    elif i == 3:
+        file_name = inputs_list[3]
+    elif i == 4:
+        file_name = inputs_list[4]
+
+# print(file_name)
+# cons = []
+# con = ""
+with open(file_name) as r:
+    try:
+        cons = r.read()
+        # print(cons)
+        # con += cons
+    except:
+        pass
+i = i + 1
+
+# cons.replace("'", "[")
+li = cons.split('\n')
+li2 = [e.split(',') for e in li]
+g = []
+for e in li2:
+    li4 = []
+    for j in e:
+        li4.append(int(j))
+    g.append(li4)
+# print(li3)
 
 df = pd.DataFrame(g)
 
@@ -42,12 +91,16 @@ while edges < nodes - 1:
                         a = m
                         b = n
     out += [[str(a) + " - " + str(b)] + [str(g[a][b])]]
-    df2 = pd.DataFrame(out,columns=["Edge", "Weight"])
+    df2 = pd.DataFrame(out, columns=["Edge", "Weight"])
     # print(df2)
     # print(out)
     selected_node[b] = True
     edges += 1
 
-print(df2)
-print(selected_node)
-
+try:
+    print("The dataframe for minimum spanning tree graph is: ")
+    print(df2)
+    # print(df2.values.tolist())
+    print(selected_node)
+except:
+    print("Enter appropriate values for nodes and edges")
