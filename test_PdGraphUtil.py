d74a074c1b56ebd5c42013295d43051ec2e74ec8
cherishdayala <35199567+cherishdayala@users.noreply.github.com>
2021-12-01 12:57:04 +0530

    Updating the path for test files in input

--- test_PdGraphUtil.py
+++ test_PdGraphUtil.py
@@ -46,7 +46,7 @@ class TestGraph(unittest.TestCase):
 
     def test_get_edgelist_from_graph(self):
         script_dir = os.path.dirname(__file__)
-        input_file = script_dir + "/Datasets/graph1list.txt"
+        input_file = script_dir + "/Classes/DataSets/graph1list.txt"
         edge_list = get_edgelist_from_graph(input_file)
         expected_list = np.array([[1, 2], [1, 3], [2, 1], [2, 3], [2, 4], [2, 5], [
                                  3, 1], [3, 2], [3, 4], [4, 2], [4, 3], [4, 5], [5, 2], [5, 4]])
@@ -54,7 +54,7 @@ class TestGraph(unittest.TestCase):
 
     def test_get_adj_mat(self):
         script_dir = os.path.dirname(__file__)
-        input_file = script_dir + "/Datasets/graph1list.txt"
+        input_file = script_dir + "/Classes/DataSets/graph1list.txt"
         adj_mat = get_adj_mat(input_file)
         expected_adj_mat = np.array([[0, 1, 1, 0, 0], [1, 0, 1, 1, 1], [
                                     1, 1, 0, 1, 0], [0, 1, 1, 0, 1], [0, 1, 0, 1, 0]])
@@ -63,14 +63,14 @@ class TestGraph(unittest.TestCase):
 
     def test_csv_to_numpy_adjmat(self):
         script_dir = os.path.dirname(__file__)
-        input_file_unweighted = script_dir + "/Datasets/graph1mat.csv"
+        input_file_unweighted = script_dir + "/Classes/DataSets/graph1mat.csv"
         adj_mat_unweighted = csv_to_numpy_adjmat(input_file_unweighted)
         expected_unweighted = np.array([[0, 1, 1, 0, 0], [1, 0, 1, 1, 1], [
                                        1, 1, 0, 1, 0], [0, 1, 1, 0, 1], [0, 1, 0, 1, 0]])
         self.assertTrue(np.array_equal(
             adj_mat_unweighted, expected_unweighted))
 
-        input_file_weighted = script_dir + "/Datasets/graph1weightedMat.csv"
+        input_file_weighted = script_dir + "/Classes/DataSets/graph1weightedMat.csv"
         adj_mat_weighted = csv_to_numpy_adjmat(input_file_weighted)
         expected_weighted = np.array([[0, 3, 7, 0, 0], [3, 0, 2, 2, 4], [
                                      1, 2, 0, 1, 0], [0, 2, 1, 0, 10], [0, 4, 0, 10, 0]])
